655f9f7fe33b9fe8c66fe9d64701551d8843813a
stygian-violet <221895403+stygian-violet@users.noreply.github.com>
2025-09-12 08:53:28 +0000

    use atomic int to count hyphae

--- internal/hyphae/count.go
+++ internal/hyphae/count.go
@@ -1,39 +1,28 @@
 package hyphae
 
 import (
-	"sync"
+	"sync/atomic"
 )
 
 // Its value is number of all existing hyphae. NonEmptyHypha mutators are expected to manipulate the value. It is concurrent-safe.
-var count = struct {
-	value int
-	sync.Mutex
-}{}
+var count atomic.Int32
 
 // ResetCount sets the value of hyphae count to zero. Use when reloading hyphae.
 func ResetCount() {
-	count.Lock()
-	count.value = 0
-	count.Unlock()
+	count.Store(0)
 }
 
 // Count how many hyphae there are. This is a O(1), the number of hyphae is stored in memory.
 func Count() int {
-	count.Lock()
-	defer count.Unlock()
-	return count.value
+	return int(count.Load())
 }
 
 // incrementCount increments the value of the hyphae counter. Use when creating new hyphae or loading hyphae from disk.
 func incrementCount() {
-	count.Lock()
-	count.value++
-	count.Unlock()
+	count.Add(1)
 }
 
 // decrementCount decrements the value of the hyphae counter. Use when deleting existing hyphae.
 func decrementCount() {
-	count.Lock()
-	count.value--
-	count.Unlock()
+	count.Add(-1)
 }
